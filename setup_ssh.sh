ca63ff965b2a1ee5b674babf511ecf2f0ce6a009
uttarayan21 <uttarayan21@gmail.com>
2017-11-15 02:30:05 +0530

    v-1.3

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -4,5 +4,15 @@ echo Adding ssh-agent to termux boot
 mkdir -p ~/.termux/boot
 cp ssh-agent ~/.termux/boot/ssh-agent
 
-echo Do you want to make a new ssh key ? \(y/n\)
+read -n1 -p "Do you want to make a new ssh key ? \(y/n\)" a
+if [[ $a == "y" | $a == "Y"]]
+ then
+  read -p "Please type your email" eml
+  if [[ ! -d ~/.ssh ]]
+    then mkdir ~/.ssh
+    CD = "$PWD"
+  fi
+  cd ~/.ssh
+  ssh-keygen -t rsa -b 4096 -C "$eml"
+
 
