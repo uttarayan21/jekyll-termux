eb8ffb278d0e4152f0ee97662294217d7ad08aff
uttarayan21 <uttarayan21@gmail.com>
2018-02-17 05:59:59 +0530

    test

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 echo Jekyll termux installer
-echo Do you want to replace the bashrc \(recommended\)
-read a
+read -p "Do you want to replace the bashrc \(recommended\)" a
 if [[ $a = "y" ]]
  then
   if [ -e ~/.bashrc ]; then
@@ -9,16 +8,15 @@ if [[ $a = "y" ]]
   cp bashrc ~/.bashrc
 fi
 unset a
-echo Install required packages ? \(about 70mb total in size use if you want to view the page locally\)
-read a
+
+read -p "Install required packages ? \(about 70mb total in size use if you want to view the page locally\)" a
 if [[ $a == "y" ]]
  then
   apt install -y ruby-dev llvm libffi-dev python git bash-completion
   gem install jekyll
 fi
 unset a
-echo Do you want to setup ssh now ?\(Needed for not entering password everytime\) \(y/n\)
-read a
+read -p "Do you want to setup ssh now ? \(y/n\)" a
 if [[ $a == "y" ]]
  then
   unset a
--- setup_ssh.sh
+++ setup_ssh.sh
@@ -4,5 +4,39 @@ echo Adding ssh-agent to termux boot
 mkdir -p ~/.termux/boot
 cp ssh-agent ~/.termux/boot/ssh-agent
 
-echo Do you want to make a new ssh key ? \(y/n\)
+default_key=git_rsa
 
+read -n 1 -p "Do you want to make a new ssh key ? (y/n) " a
+echo
+if [[ $a == "y" ]];then
+  read -p "Please enter your GitHub.com email address : " email
+  read -p "Please enter your GitHub.com username" username
+  read -n 1 -p "Do you want to change the name of the file ? (y/n) (Default is $default_key) "
+  if [[ $a == "y" ]];then
+    read -p "Enter the new name for the key without the path" default_key
+    if [[ $default_key == "" ]];then
+      echo "The name of the name of the key can't be blank "
+      exit 1
+    fi
+  fi
+  ssh-keygen -t rsa -b 4096 -f ~/.ssh/git_rsa -C $email
+else
+  exit 1
+fi
+unset a
+read -n 1 -p "Do you want to add the public key to your GitHub account ? (y/n) : " a
+if [[ $a == "y" ]];then
+  cat ~/.ssh/git_rsa.pub | termux-clipboard-set
+  echo "Copied the pub key to clipboard"
+  echo "Paste it in the key block of the opened page"
+  echo "And type in a title"
+  xdg-open https://github.com/settings/ssh/new
+  echo "Once it's added press anykey"
+  read -s -n 1 a
+  unset a
+  echo Testing the connection
+  ssh -T git@github.com
+  if [[ $? != 1 ]];then
+    echo "An unexpected error occured please report this problem to github.com/jekyll-termux/issues"
+  fi
+fi
