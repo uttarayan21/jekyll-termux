1c30eb76592e936d3424c5de6b7135de0c5d4a31
uttarayan21 <uttarayan21@gmail.com>
2018-03-07 21:19:27 +0530

    fix script not working

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ unset a
 read -n 1 -p "Install required packages ? (about 70mb total in size use if you want to view the page locally)" a
 if [[ $a == "y" ]]
  then
-  apt install -y ruby-dev llvm libffi-dev python git bash-completion
+  apt install -y ruby-dev llvm libffi-dev python git bash-completion termux-api make clang
   gem install jekyll
 fi
 unset a
